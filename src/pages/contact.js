a8d70a7633ba33a1c68d4e4da899a9cbcd1fb285
VarnitS2 <varnits02@gmail.com>
2020-05-24 03:13:07 -0400

    Added a link to my email in the contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const ContactPage = () => {
     return (
         <div>
             <h1>Contact Me</h1>
             <p>
-                The best way to reach me is via email at varnits2@illinois.edu.
+                The best way to reach me is via email at <a href="mailto:varnits2@illinois.edu">varnits2@illinois.edu</a>.
             </p>
         </div>
     )
