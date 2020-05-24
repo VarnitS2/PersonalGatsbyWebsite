63637dda26ef79c477080c3b870c7a22297c8cab
VarnitS2 <varnits02@gmail.com>
2020-05-24 03:05:03 -0400

    Added a contact me page

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <p>
+                The best way to reach me is via email at varnits2@illinois.edu.
+            </p>
+        </div>
+    )
+}
+
+export default ContactPage
