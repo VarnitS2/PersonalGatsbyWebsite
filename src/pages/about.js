5732e52bb0eed1b0075ce48774953e49ad6efb5f
VarnitS2 <varnits02@gmail.com>
2020-05-24 03:03:11 -0400

    Created an about page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                Hi! My name is Varnit Sinha and I am a student at the University of Illinois at Urbana Chamapaign. I am majoring in Engineering Physics with a concentration in Computational Physics, and I am minoring in Mathematics and Computer Science.
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage
