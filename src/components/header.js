c717d37ecb63dd140279993cedce2a3ab79930cc
VarnitS2 <varnits02@gmail.com>
2020-05-28 22:32:21 -0400

    Added GitHub and LinkedIn images with links.

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 
 import headerStyles from "./header.module.scss"
+import githubLogo from "../assets/github.jpg"
+import linkedinLogo from "../assets/linkedin.jpg"
 
 const Header = () => {
     return (
@@ -22,6 +24,14 @@ const Header = () => {
                     <li>
                         <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
                     </li>
+                    <ul className={headerStyles.navLogo}>
+                        <li>
+                            <a href="https://github.com/VarnitS2" target="_blank"><img src={githubLogo} alt="GitHub" /></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/varnits/" target="_blank"><img className={headerStyles.navLogoItem} src={linkedinLogo} alt="LinkedIn" /></a>
+                        </li>
+                    </ul>
                 </ul>
             </nav>
         </header>
